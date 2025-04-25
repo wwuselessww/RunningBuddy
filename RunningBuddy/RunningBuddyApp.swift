c59e82e08f025bfb04f355e7319c38403dce591e
wwuselessww <sa24122@gmail.com>
2025-04-25 21:08:02 +0300

    Initial Commit

---
+++ RunningBuddy/RunningBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningBuddyApp.swift
+//  RunningBuddy
+//
+//  Created by Alexander Kozharin on 25.04.25.
+//
+
+import SwiftUI
+
+@main
+struct RunningBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
