ac0491a4db50ecca1e86593bea8325370f9d72c2
wwuselessww <sa24122@gmail.com>
2025-07-01 20:59:12 +0300

    refactored functions to get pulse and removed some debug prints and comments

---
+++ RunningBuddy/Models/StatisticType.swift
@@ -0,0 +1,14 @@
+//
+//  StatisticType.swift
+//  RunningBuddy
+//
+//  Created by Alexander Kozharin on 01.07.25.
+//
+
+import Foundation
+
+enum StatisticType {
+    case min
+    case avg
+    case max
+}
